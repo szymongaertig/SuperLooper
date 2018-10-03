6b2ff8ac82d0629ba5d8de6b9d5ba0581065a078
garfieldsg <szymon@gaertig.com.pl>
2018-10-03 16:50:03 +0200

    Add project files.

---
+++ MidiController/MidiController.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		MidiController.ino
+ Created:	10/3/2018 4:49:51 PM
+ Author:	Garfieldsg
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
